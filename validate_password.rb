d8938f8049d24eeb0d7bf37a6c8b7ed05cd32be4
Paulina Szarek <xpaula20@wp.pl>
2023-10-04 22:25:51 +0200

    adding a solution to the task for workshops

--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,22 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    return false if @password.length < 6 || @password.length > 24
+    return false unless @password =~ /[A-Z]/
+    return false unless @password =~ /[a-z]/
+    return false unless @password =~ /\d/
+    return false unless @password =~ /[!@#$%&*+=:;?<>\[\]{}()|]/
+    return false if has_repeating_characters?(2)
+    true
+  end
+
+  private
+
+  def has_repeating_characters?(max_repeats)
+    regex = /(.).*\1{#{max_repeats},}/
+    !(@password =~ regex).nil?
+  end
 end
